865cad998fa784ab3e438ef3e3062e0f4f9a234d
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2023-03-29 12:50:01 +0300

    update, delete

--- src/main/java/com/phonebook/myPhonebook/controller/ContactController.java
+++ src/main/java/com/phonebook/myPhonebook/controller/ContactController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -21,18 +22,39 @@ public class ContactController {
     @GetMapping("/contacts")
     public String findAll(Model model) {
         List<Contact> contacts = contactsctervice.findAll();
-        model.addAttribute("contacts",contacts);
+        model.addAttribute("contacts", contacts);
         return "contact-list";
     }
+
     @GetMapping("/contact-create")
-    public String createContactForm (Contact contact){
+    public String createContactForm(Contact contact) {
         return "contact-create";
 
     }
-    @PostMapping
-    public String createContact(Contact contact){
-        contactsctervice.saveContact(contact);
-        return "redirect:/contscts";
 
+    @PostMapping("/contact-create")
+    public String createContact(Contact contact) {
+        contactsctervice.saveContact(contact);
+        return "redirect:/contacts";
+
+    }
+
+    @GetMapping("/contact-delete/{id}")
+    public String deleteContact(@PathVariable("id") Long id) {
+        contactsctervice.deletByID(id);
+        return "redirect:/contacts";
+    }
+
+    @GetMapping("contact-update/{id}")
+    public String updateContactForm(@PathVariable("id") Long id, Model model) {
+        Contact contact = contactsctervice.findByID(id);
+        model.addAttribute("contact",contact);
+        return "/contact-update";
+    }
+
+    @PostMapping("/contact-update")
+    public String updateContact(Contact contact) {
+        contactsctervice.saveContact(contact);
+        return "redirect:/contacts";
     }
 }
